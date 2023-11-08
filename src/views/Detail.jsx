8b349cb01d1f0b0679b26818fa029a46c4e61011
Jose Burgos <jose.b1996m@gmail.com>
2023-11-08 09:16:02 -0500

    [feature] back button to go back to home

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,15 +1,25 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useGetImage from "../hooks/useGetImage.js";
 import ImageDetail from "../components/ImageDetail.jsx";
+import { IoArrowBackSharp } from "react-icons/io5";
 
 function Detail () {
     const { imageId } = useParams();
     const { data, loading } = useGetImage('photos', imageId)
 
     return (
-        <div className="container d-flex justify-center">
-            {loading ? <p>Loading data...</p> : <ImageDetail data={data}/>}
-        </div>
+        <>
+            <div className="container">
+                <Link to="/" className="link-d-none">
+                    <div className="backBtn">
+                        <IoArrowBackSharp className="vertical-top" /> Back
+                    </div>
+                </Link>
+            </div>
+            <div className="container d-flex justify-center">
+                {loading ? <p>Loading data...</p> : <ImageDetail data={data}/>}
+            </div>
+        </>
     )
 }
 
